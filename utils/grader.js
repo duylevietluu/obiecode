1394f6fc0e8978f705f631211dba39d5355c01cb
Duy Le <dle2@oberlin.edu>
2023-07-15 17:10:45 +0700

    2nd commit: fix python by introducing a seperate python API

--- utils/grader.js
+++ utils/grader.js
@@ -1,17 +1,12 @@
-import { gradePython } from "./pythonWorker";
-
 export const grader = async (code, testCases) => {
-  const results = [];
-  let grade = 0;
-  for (let i = 0; i < testCases.length; i++) {
-    const testCase = testCases[i];
-    // use gradePython
-    const result = await gradePython(code, testCase.input, testCase.output);
-    results.push(result);
-    if (result.success) {
-      grade++;
-    }
-  }
-  grade = Math.round(grade / testCases.length * 10000) / 100;
-  return { results, grade };
+  const response = await fetch(process.env.PYTHON_RUN_PATH, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-store' // Disable caching
+    },
+    body: JSON.stringify({ code, testCases })
+  });
+  const data = await response.json();
+  return data;
 }
